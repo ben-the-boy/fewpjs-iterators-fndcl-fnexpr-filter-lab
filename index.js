c6eeaed240149ed79f22ba673a46d084d07c1fef
Ben Hayes <b.t.hayes@comcast.net>
2021-07-07 15:56:42 -0500

    commit tree 0ef233629d4f4f4a378e169af6d57d3933fee452

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  let matches = drivers.filter(n => {
+    return n.toLowerCase() === name.toLowerCase();
+  })
+  return matches;
+}
+
+function fuzzyMatch(drivers, letters) {
+  let matches = drivers.filter(n => {
+    return n.slice(0, letters.length) === letters;
+  })
+  return matches;
+}
+
+function matchName(drivers, name) {
+  let matches = drivers.filter(n => {
+    return n.name === name;
+  })
+  return matches;
+}
